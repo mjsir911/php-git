70f49a1a8d496b90557888d08a207a3da91c23ae
Marco Sirabella <marco@sirabella.org>
2021-04-04 16:08:07 -0700

    fixup! Objectify commits

--- src/commit.stub.php
+++ src/commit.stub.php
@@ -5,16 +5,16 @@ namespace git {
 		public function __construct(Repository $repo, mixed $oid) {}
 
 		// these are propreties and I would love to make an object
-		public function id(mixed $commit): mixed {}
-		public function message_encoding(mixed $commit): string {}
-		public function message(mixed $commit): string {}
-		public function summary(mixed $commit): string {}
+		public function id(): mixed {}
+		public function message_encoding(): string {}
+		public function message(): string {}
+		public function summary(): string {}
 		// public function time(mixed $commit): mixed {}
 		// public function time_offset(mixed $commit): int {}
 		// public function commiter(mixed $commit): mixed {}
 		// public function author(mixed $commit): mixed {}
 		// public function raw_header(mixed $commit): string {}
-		public function tree_id(mixed $commit): mixed {}
+		public function tree_id(): mixed {}
 	}
 }
 ?>
