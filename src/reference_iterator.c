2d80595cd5613de6bf21c5d471f93369be91d26b
Marco Sirabella <marco@sirabella.org>
2021-04-06 22:48:48 -0700

    Move $repo->references() getter to reference_iterator.c

--- src/reference_iterator.c
+++ src/reference_iterator.c
@@ -4,6 +4,7 @@
 #include <git2/errors.h>
 #include <git2/repository.h>
 #include "reference_iterator.h"
+#include "repository.h"
 #include "reference.h"
 #include "error.h"
 
@@ -29,7 +30,29 @@ void php_git2_reference_iterator_free(zend_object *obj) {
 	zend_object_std_dtor(&iter->std);
 }
 
-#include "repository.h"
+
+ZEND_METHOD(git_Repository, references) {
+	zend_string *glob = NULL;
+
+	ZEND_PARSE_PARAMETERS_START(0, 1)
+		Z_PARAM_OPTIONAL
+		Z_PARAM_STR_OR_NULL(glob)
+	ZEND_PARSE_PARAMETERS_END();
+
+
+	object_init_ex(return_value, reference_iterator_class_entry);
+	reference_iterator_t *iter = Z_REFERENCE_ITERATOR_P(return_value);
+
+	repository_t *repo = Z_REPOSITORY_P(ZEND_THIS);
+
+	if (glob)
+		git_reference_iterator_glob_new(&iter->reference_iterator, repo->repo, ZSTR_VAL(glob));
+	else
+		git_reference_iterator_new(&iter->reference_iterator, repo->repo);
+
+	// git_reference_next(&iter->current, iter->reference_iterator); // do it once
+	zend_call_method_with_0_params(Z_OBJ_P(return_value), Z_OBJCE_P(return_value), NULL, "next", NULL);
+}
 
 ZEND_METHOD(git_ReferenceIterator, current) {
 	ZEND_PARSE_PARAMETERS_NONE();
--- src/repository.c
+++ src/repository.c
@@ -119,28 +119,3 @@ ZEND_METHOD(git_Repository, commondir) {
 
 	RETURN_STRING(git_repository_commondir(repo->repo));
 }
-
-#include "reference_iterator.h"
-
-ZEND_METHOD(git_Repository, references) {
-	zend_string *glob = NULL;
-
-	ZEND_PARSE_PARAMETERS_START(0, 1)
-		Z_PARAM_OPTIONAL
-		Z_PARAM_STR_OR_NULL(glob)
-	ZEND_PARSE_PARAMETERS_END();
-
-
-	object_init_ex(return_value, reference_iterator_class_entry);
-	reference_iterator_t *iter = Z_REFERENCE_ITERATOR_P(return_value);
-
-	repository_t *repo = Z_REPOSITORY_P(ZEND_THIS);
-
-	if (glob)
-		git_reference_iterator_glob_new(&iter->reference_iterator, repo->repo, ZSTR_VAL(glob));
-	else
-		git_reference_iterator_new(&iter->reference_iterator, repo->repo);
-
-	// git_reference_next(&iter->current, iter->reference_iterator); // do it once
-	zend_call_method_with_0_params(Z_OBJ_P(return_value), Z_OBJCE_P(return_value), NULL, "next", NULL);
-}
